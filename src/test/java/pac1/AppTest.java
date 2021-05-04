35331457e61a7755f37c8956f0c3d6a7c7202a24
deepak <dpanda178@gmail.com>
2021-05-04 20:34:10 +0530

    this is first commit

---
+++ src/test/java/pac1/AppTest.java
@@ -0,0 +1,19 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+//import static org.junit.Assert.assertTrue;
+
+//import org.junit.Test;
+
+
+public class AppTest 
+{
+    
+    @Test
+    public void launchBrowser()
+    {
+        System.out.println("launch browser");
+        
+    }
+}
